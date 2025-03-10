954f4ece6addf81f7ef0f824f2796c0e2969d049
nhatphanhk <114910072+nhatphanhk@users.noreply.github.com>
2025-03-10 15:22:24 +0700

    test(admin): remove password update test case from admin update flow

--- test/admin/users.e2e-spec.ts
+++ test/admin/users.e2e-spec.ts
@@ -17,10 +17,10 @@ describe('Users Module', () => {
   });
 
   describe('Update', () => {
+    let newUser;
     const newUserEmail = `user-first.${Date.now()}@example.com`;
     const newUserChangedEmail = `user-first-changed.${Date.now()}@example.com`;
     const newUserPassword = `secret`;
-    const newUserChangedPassword = `new-secret`;
 
     beforeAll(async () => {
       await request(app)
@@ -31,22 +31,26 @@ describe('Users Module', () => {
           firstName: `First${Date.now()}`,
           lastName: 'E2E',
         });
+
+      await request(app)
+        .post('/api/v1/auth/email/login')
+        .send({ email: newUserEmail, password: newUserPassword })
+        .then(({ body }) => {
+          newUser = body.user;
+        });
     });
 
     describe('User with "Admin" role', () => {
-      describe('Guest', () => {
-        it('should login with changed password: /api/v1/auth/email/login (POST)', () => {
-          return request(app)
-            .post('/api/v1/auth/email/login')
-            .send({
-              email: newUserChangedEmail,
-              password: newUserChangedPassword,
-            })
-            .expect(200)
-            .expect(({ body }) => {
-              expect(body.token).toBeDefined();
-            });
-        });
+      it('should update email for existing user: /api/v1/users/:id (PATCH)', () => {
+        return request(app)
+          .patch(`/api/v1/users/${newUser.id}`)
+          .auth(apiToken, {
+            type: 'bearer',
+          })
+          .send({
+            email: newUserChangedEmail,
+          })
+          .expect(200);
       });
     });
   });
